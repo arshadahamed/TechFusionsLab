a51346ec9fdb685f53270f4fadd78371b26be7f4
arshadahamed <arshadpayoneer@gmail.com>
2025-09-14 03:18:04 +0530

    Modified Contact page's map iframe link

--- techfusionslab/resources/views/home/contact/contact_page.blade.php
+++ techfusionslab/resources/views/home/contact/contact_page.blade.php
@@ -128,7 +128,7 @@
                     <div class="gt-map-items">
                         <div class="googpemap">
                             <iframe
-                                src="{{ $companyInfo->map_link ?? 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.902632703823!2d90.4066326755292!3d23.750903894634736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b0b9f5a6b7%3A0x8e7c4f4e4f4e4f4e!2sTechFusions%20Lab%20-%20Web%20Design%20%26%20Development%20Company%20in%20Bangladesh!5e0!3m2!1sen!2sbd!4v1696112345678!5m2!1sen!2sbd' }}"
+                                src="{{ $companyInfo->map_iframe }}"
                                 style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                         </div>
                     </div>
